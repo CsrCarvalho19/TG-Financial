564e6b508c078d613cf2c4a0d0e79c1c86e83234
Cesar Augusto de Carvalho Silva <CIANDT\cesars@lnb028195cps.ciandt.global>
2021-02-28 19:23:18 -0300

    Development environment set up

---
+++ src/test/java/br/com/tgfinancial/tgfinancial/TgFinancialApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tgfinancial.tgfinancial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgFinancialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
