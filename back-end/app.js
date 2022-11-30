7557e79c50e879b430428dab8c08df6929c91ea2
Cesar Carvalho <cesar.carvalho19@live.com>
2022-11-29 21:09:19 -0300

    Some updates and tests.

--- back-end/app.js
+++ back-end/app.js
@@ -6,7 +6,16 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const db = require('./config/database')
+const dbUser = process.env.DB_USER
+const dbPass = process.env.DB_PASS
+const dbName = process.env.DB_NAME
+db(`mongodb+srv://financialTG:AlNNiR5WwCv5xGhJ@cluster0.0xp9e.mongodb.net/financial?retryWrites=true&w=majority`)
+
 var app = express();
+
+const cors = require('cors')
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +25,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const gasto = require('./routes/gasto')
+app.use('/gasto', gasto)
+
+const recebimento = require('./routes/recebimento')
+app.use('/recebimento', recebimento)
+
+const usuario = require('./routes/usuario')
+app.use('/usuario', usuario)
 
 module.exports = app;
