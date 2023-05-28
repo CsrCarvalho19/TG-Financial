17860b3bceaccb7af27e2edcb45d4a41ed9834e4
Cesar Augusto de Carvalho Silva <cesar.carvalho19@live.com>
2023-05-27 22:15:10 -0300

    Adjust imports.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { MaterialModule } from './material/material.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
